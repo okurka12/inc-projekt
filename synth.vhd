c705239405242f48e2852da2d1f8909f5b2dc343
Vit Pavlik <pavlikvitek13@gmail.com>
2023-04-08 16:12:00 +0200

    sampling every 16 clock cycles done

--- synth.vhd
+++ synth.vhd
@@ -24,31 +24,31 @@ end entity counter_4;
 
 architecture rtl of counter_4 is
   signal cnt_sig : std_logic_vector (3 downto 0);
-  signal n57_o : std_logic;
-  signal n60_o : std_logic;
-  signal n62_o : std_logic_vector (3 downto 0);
-  signal n64_o : std_logic_vector (3 downto 0);
-  signal n67_o : std_logic_vector (3 downto 0);
-  signal n68_q : std_logic_vector (3 downto 0) := "0000";
+  signal n67_o : std_logic;
+  signal n70_o : std_logic;
+  signal n72_o : std_logic_vector (3 downto 0);
+  signal n74_o : std_logic_vector (3 downto 0);
+  signal n77_o : std_logic_vector (3 downto 0);
+  signal n78_q : std_logic_vector (3 downto 0) := "0000";
 begin
   cnt_out <= cnt_sig;
-  -- uart_rx.vhd:50:8
-  cnt_sig <= n68_q; -- (isignal)
-  -- uart_rx.vhd:64:13
-  n57_o <= '1' when rising_edge (cnt_in) else '0';
-  -- uart_rx.vhd:65:25
-  n60_o <= '1' when cnt_sig = "1111" else '0';
-  -- uart_rx.vhd:68:36
-  n62_o <= std_logic_vector (unsigned (cnt_sig) + unsigned'("0001"));
-  -- uart_rx.vhd:65:13
-  n64_o <= n62_o when n60_o = '0' else "0000";
-  -- uart_rx.vhd:38:8
-  n67_o <= cnt_sig when cnt_ce = '0' else n64_o;
-  -- uart_rx.vhd:64:9
+  -- uart_rx.vhd:79:8
+  cnt_sig <= n78_q; -- (isignal)
+  -- uart_rx.vhd:93:13
+  n67_o <= '1' when rising_edge (cnt_in) else '0';
+  -- uart_rx.vhd:94:25
+  n70_o <= '1' when cnt_sig = "1111" else '0';
+  -- uart_rx.vhd:97:36
+  n72_o <= std_logic_vector (unsigned (cnt_sig) + unsigned'("0001"));
+  -- uart_rx.vhd:94:13
+  n74_o <= n72_o when n70_o = '0' else "0000";
+  -- uart_rx.vhd:39:8
+  n77_o <= cnt_sig when cnt_ce = '0' else n74_o;
+  -- uart_rx.vhd:93:9
   process (cnt_in)
   begin
     if rising_edge (cnt_in) then
-      n68_q <= n67_o;
+      n78_q <= n77_o;
     end if;
   end process;
 end rtl;
@@ -65,58 +65,80 @@ entity demux_4_8 is
 end entity demux_4_8;
 
 architecture rtl of demux_4_8 is
-  signal n18_o : std_logic;
-  signal n19_o : std_logic;
-  signal n22_o : std_logic;
-  signal n23_o : std_logic;
-  signal n26_o : std_logic;
-  signal n27_o : std_logic;
-  signal n30_o : std_logic;
-  signal n31_o : std_logic;
-  signal n34_o : std_logic;
-  signal n35_o : std_logic;
-  signal n38_o : std_logic;
-  signal n39_o : std_logic;
-  signal n42_o : std_logic;
-  signal n43_o : std_logic;
-  signal n46_o : std_logic;
-  signal n47_o : std_logic;
-  signal n49_o : std_logic_vector (7 downto 0);
+  signal n28_o : std_logic;
+  signal n29_o : std_logic;
+  signal n32_o : std_logic;
+  signal n33_o : std_logic;
+  signal n36_o : std_logic;
+  signal n37_o : std_logic;
+  signal n40_o : std_logic;
+  signal n41_o : std_logic;
+  signal n44_o : std_logic;
+  signal n45_o : std_logic;
+  signal n48_o : std_logic;
+  signal n49_o : std_logic;
+  signal n52_o : std_logic;
+  signal n53_o : std_logic;
+  signal n56_o : std_logic;
+  signal n57_o : std_logic;
+  signal n59_o : std_logic_vector (7 downto 0);
 begin
-  dm_out <= n49_o;
-  -- uart_rx.vhd:84:34
-  n18_o <= '1' when addr = "0000" else '0';
-  -- uart_rx.vhd:84:24
-  n19_o <= '0' when n18_o = '0' else dm_in;
-  -- uart_rx.vhd:85:34
-  n22_o <= '1' when addr = "0001" else '0';
-  -- uart_rx.vhd:85:24
-  n23_o <= '0' when n22_o = '0' else dm_in;
-  -- uart_rx.vhd:86:34
-  n26_o <= '1' when addr = "0010" else '0';
-  -- uart_rx.vhd:86:24
-  n27_o <= '0' when n26_o = '0' else dm_in;
-  -- uart_rx.vhd:87:34
-  n30_o <= '1' when addr = "0011" else '0';
-  -- uart_rx.vhd:87:24
-  n31_o <= '0' when n30_o = '0' else dm_in;
-  -- uart_rx.vhd:88:34
-  n34_o <= '1' when addr = "0100" else '0';
-  -- uart_rx.vhd:88:24
-  n35_o <= '0' when n34_o = '0' else dm_in;
-  -- uart_rx.vhd:89:34
-  n38_o <= '1' when addr = "0101" else '0';
-  -- uart_rx.vhd:89:24
-  n39_o <= '0' when n38_o = '0' else dm_in;
-  -- uart_rx.vhd:90:34
-  n42_o <= '1' when addr = "0110" else '0';
-  -- uart_rx.vhd:90:24
-  n43_o <= '0' when n42_o = '0' else dm_in;
-  -- uart_rx.vhd:91:34
-  n46_o <= '1' when addr = "0111" else '0';
-  -- uart_rx.vhd:91:24
-  n47_o <= '0' when n46_o = '0' else dm_in;
-  n49_o <= n47_o & n43_o & n39_o & n35_o & n31_o & n27_o & n23_o & n19_o;
+  dm_out <= n59_o;
+  -- uart_rx.vhd:114:34
+  n28_o <= '1' when addr = "0000" else '0';
+  -- uart_rx.vhd:114:24
+  n29_o <= '0' when n28_o = '0' else dm_in;
+  -- uart_rx.vhd:115:34
+  n32_o <= '1' when addr = "0001" else '0';
+  -- uart_rx.vhd:115:24
+  n33_o <= '0' when n32_o = '0' else dm_in;
+  -- uart_rx.vhd:116:34
+  n36_o <= '1' when addr = "0010" else '0';
+  -- uart_rx.vhd:116:24
+  n37_o <= '0' when n36_o = '0' else dm_in;
+  -- uart_rx.vhd:117:34
+  n40_o <= '1' when addr = "0011" else '0';
+  -- uart_rx.vhd:117:24
+  n41_o <= '0' when n40_o = '0' else dm_in;
+  -- uart_rx.vhd:118:34
+  n44_o <= '1' when addr = "0100" else '0';
+  -- uart_rx.vhd:118:24
+  n45_o <= '0' when n44_o = '0' else dm_in;
+  -- uart_rx.vhd:119:34
+  n48_o <= '1' when addr = "0101" else '0';
+  -- uart_rx.vhd:119:24
+  n49_o <= '0' when n48_o = '0' else dm_in;
+  -- uart_rx.vhd:120:34
+  n52_o <= '1' when addr = "0110" else '0';
+  -- uart_rx.vhd:120:24
+  n53_o <= '0' when n52_o = '0' else dm_in;
+  -- uart_rx.vhd:121:34
+  n56_o <= '1' when addr = "0111" else '0';
+  -- uart_rx.vhd:121:24
+  n57_o <= '0' when n56_o = '0' else dm_in;
+  n59_o <= n57_o & n53_o & n49_o & n45_o & n41_o & n37_o & n33_o & n29_o;
+end rtl;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity eq_4 is
+  port (
+    eq_in_1 : in std_logic_vector (3 downto 0);
+    eq_in_2 : in std_logic_vector (3 downto 0);
+    eq_out : out std_logic);
+end entity eq_4;
+
+architecture rtl of eq_4 is
+  signal n21_o : std_logic;
+  signal n24_o : std_logic;
+begin
+  eq_out <= n24_o;
+  -- uart_rx.vhd:66:21
+  n21_o <= '1' when eq_in_1 = eq_in_2 else '0';
+  -- uart_rx.vhd:66:9
+  n24_o <= '0' when n21_o = '0' else '1';
 end rtl;
 
 library ieee;
@@ -150,11 +172,14 @@ architecture rtl of uart_rx is
   signal wrap_DOUT_VLD: std_logic;
   signal din_not : std_logic;
   signal dmx_address : std_logic_vector (3 downto 0);
+  signal clk_cnt : std_logic_vector (3 downto 0);
   signal fsm_test_out : std_logic;
+  signal clk_eq_15_eq_out : std_logic;
+  constant n5_o : std_logic_vector (3 downto 0) := "1111";
   signal dmx_dm_out : std_logic_vector (7 downto 0);
-  signal cnt_cnt_out : std_logic_vector (3 downto 0);
-  constant n6_o : std_logic := '1';
-  signal n10_o : std_logic;
+  signal cnt_clk_cnt_out : std_logic_vector (3 downto 0);
+  constant n8_o : std_logic := '1';
+  signal n12_o : std_logic;
 begin
   wrap_clk <= clk;
   wrap_rst <= rst;
@@ -162,28 +187,35 @@ begin
   dout <= wrap_dout;
   dout_vld <= wrap_dout_vld;
   wrap_DOUT <= dmx_dm_out;
-  wrap_DOUT_VLD <= fsm_test_out;
-  -- uart_rx.vhd:103:12
-  din_not <= n10_o; -- (isignal)
-  -- uart_rx.vhd:106:12
-  dmx_address <= cnt_cnt_out; -- (isignal)
-  -- uart_rx.vhd:111:5
+  wrap_DOUT_VLD <= clk_eq_15_eq_out;
+  -- uart_rx.vhd:129:12
+  din_not <= n12_o; -- (isignal)
+  -- uart_rx.vhd:132:12
+  dmx_address <= "0000"; -- (isignal)
+  -- uart_rx.vhd:135:12
+  clk_cnt <= cnt_clk_cnt_out; -- (isignal)
+  -- uart_rx.vhd:140:5
   fsm : entity work.uart_rx_fsm port map (
     clk => wrap_CLK,
     rst => wrap_RST,
     start_bit => din_not,
-    test_out => fsm_test_out);
-  -- uart_rx.vhd:120:5
+    test_out => open);
+  -- uart_rx.vhd:149:5
+  clk_eq_15 : entity work.eq_4 port map (
+    eq_in_1 => clk_cnt,
+    eq_in_2 => n5_o,
+    eq_out => clk_eq_15_eq_out);
+  -- uart_rx.vhd:157:5
   dmx : entity work.demux_4_8 port map (
     dm_in => wrap_DIN,
     addr => dmx_address,
     dm_out => dmx_dm_out);
-  -- uart_rx.vhd:128:5
-  cnt : entity work.counter_4 port map (
+  -- uart_rx.vhd:165:5
+  cnt_clk : entity work.counter_4 port map (
     cnt_in => wrap_CLK,
-    cnt_ce => n6_o,
+    cnt_ce => n8_o,
     cnt_res => wrap_CLK,
-    cnt_out => cnt_cnt_out);
-  -- uart_rx.vhd:139:20
-  n10_o <= not wrap_DIN;
+    cnt_out => cnt_clk_cnt_out);
+  -- uart_rx.vhd:176:20
+  n12_o <= not wrap_DIN;
 end rtl;
--- uart_rx.vhd
+++ uart_rx.vhd
@@ -45,6 +45,33 @@ entity COUNTER_4 is
     );
 end entity;
 
+-- entita EQ (vizte navrh)
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+entity EQ_4 is 
+    port(
+        EQ_IN_1 : in std_logic_vector(3 downto 0);
+        EQ_IN_2 : in std_logic_vector(3 downto 0);
+        EQ_OUT : out std_logic 
+    );
+end entity;
+
+
+-- architektura EQ
+architecture behavioral of EQ_4 is 
+begin
+    process (EQ_IN_1, EQ_IN_2)
+    begin
+        if (EQ_IN_1 = EQ_IN_2) then
+            EQ_OUT <= '1';
+        else
+            EQ_OUT <= '0';
+        end if;
+    end process;
+end architecture;
+
+
 -- architektura citace
 architecture behavioral of COUNTER_4 is 
 
@@ -104,6 +131,9 @@ architecture behavioral of UART_RX is
     -- signal pro adresu DMX
     signal DMX_ADDRESS : std_logic_vector(3 downto 0) := "0000";
 
+    -- citac hodinoveho cyklu
+    signal CLK_CNT : std_logic_vector(3 downto 0) := "0000";
+
 begin
 
     -- Instance of RX FSM
@@ -111,11 +141,19 @@ begin
     port map (
         CLK => CLK,
         RST => RST,
-        START_BIT => DIN_NOT,
-        TEST_OUT => DOUT_VLD
+        START_BIT => DIN_NOT
+        -- TEST_OUT => DOUT_VLD
     );
 
-    -- demultiplexor podle navrhu
+    -- instance EQ pro CLK == 15
+    clk_eq_15: entity work.EQ_4
+    port map (
+        EQ_IN_1 => CLK_CNT,
+        EQ_IN_2 => "1111",
+        EQ_OUT => DOUT_VLD
+    );
+
+    -- instance demultiplexoru podle navrhu
     dmx: entity work.DEMUX_4_8
     port map(
         DM_IN => DIN,
@@ -123,13 +161,13 @@ begin
         DM_OUT => DOUT
     );
 
-    -- citac hodinoveho signalu
-    cnt: entity work.COUNTER_4
+    -- instance: citac hodinoveho signalu
+    cnt_clk: entity work.COUNTER_4
     port map (
         CNT_IN => CLK,
         CNT_CE => '1',
         CNT_RES => CLK,
-        CNT_OUT => DMX_ADDRESS
+        CNT_OUT => CLK_CNT
     );
 
     -- process co pri zmene DIN zmeni signal DIN_NOT
@@ -141,5 +179,18 @@ begin
     -- toto tu bylo puvodne
     -- DOUT <= (others => '0');
     -- DOUT_VLD <= '0';
+
+    -- takze kamaradickove vezte ze uz mam pocit ze vim co delam takze 
+    -- pecicka akorat s tim bude jeste dost prace ale jsem podle me 
+    -- mega dobrej ze jsem na to prisel sam takze vis co
+    -- uz akorat postupovat podle navrhu no je fakt ze ten kdybych neudelal
+    -- tak jsem ted ztracen ale nastesti ho mam i kdyz za 0 bodu!!!!!!!!
+    -- proste postup je takovy, ze deklaruji entitu, definuju jeji chovani
+    -- (architecture behavioral) a pak v te hlavni architekture vsechno
+    -- pospojuji tim ze tam ty ruzne entity dam jakozto instance 
+    -- a vsechno to pospojuji. kdyz je potreba udelat propojeni mezi instancemi
+    -- tak je na to potreba signal coz je takova ficurka tohoto
+    -- hardware popisujiciho jazyka no nic po 4 hodinach koncim praci tady na 
+    -- tomto skvelem projektu a jdu se ucit isu peace out
 
 end architecture;
